22316959ec0808255529466fab2163ed558be737
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-05-10 09:30:49 +0600

    added footer section

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
+import Footer from "../Shared/Footer/Footer";
 
 const Root = () => {
     return (
         <div className="font-lato max-w-6xl mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
