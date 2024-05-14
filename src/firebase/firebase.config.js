7523a12000b3441e07ba8e0449dfd398cac92b25
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-05-14 09:31:07 +0600

    crated firebase.confiq.js file to secure confiq keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,15 +2,15 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+console.log(import.meta.env.VITE_apiKey);
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyByZRMQu2_6hO4rxx2blKkaIQhajOaO3cM",
-  authDomain: "flavortrack-a59b2.firebaseapp.com",
-  projectId: "flavortrack-a59b2",
-  storageBucket: "flavortrack-a59b2.appspot.com",
-  messagingSenderId: "52915649094",
-  appId: "1:52915649094:web:23d1486f7fe46eaacea847"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket ,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
